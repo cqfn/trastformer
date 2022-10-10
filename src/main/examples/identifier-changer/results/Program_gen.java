960a2c21c5f248695f3ef46fd46fd0f49a04114a
Polina Volkhontseva <polina.volkhontseva@huawei.com>
2022-10-10 13:35:08 +0300

    Java code generation added to examples

---
+++ src/main/examples/identifier-changer/results/Program_gen.java
@@ -0,0 +1,5 @@
+class Program {
+    public int count(final int first, final int second) {
+        return Math.abs(first) + Math.abs(second);
+    }
+}
---
+++ src/main/examples/operator-replacer/results/Program_gen.java
@@ -0,0 +1,5 @@
+class Program {
+    public int c(final int x, final int y) {
+        return Math.abs(x) * Math.abs(y);
+    }
+}
